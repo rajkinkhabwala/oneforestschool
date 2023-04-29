cd2b09c51055b93a6e0c694d1e547a0f05410a31
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2023-04-28 18:06:15 -0700

    Removed Hosting

--- src/common/auth/auth.component.tsx
+++ src/common/auth/auth.component.tsx
@@ -2,11 +2,11 @@ import { Authenticator } from "@aws-amplify/ui-react";
 export default function Auth() {
 
   return (
-    <Authenticator
-        loginMechanisms={["email"]}
-        socialProviders={["google"]}
-        hideSignUp
-        className="authentication"
-      />
+    <>
+      <Authenticator
+      loginMechanisms={["email"]}
+      socialProviders={["google"]}
+      hideSignUp
+      className="authentication" /></>
   );
 }
