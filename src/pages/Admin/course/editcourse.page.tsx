a9d582357406581817d24a3ff880ad999b1c4740
Bhargav Shah <bhargavpshah98@gmail.com>
2023-05-03 22:38:38 -0700

    Demo Department

---
+++ src/pages/Admin/course/editcourse.page.tsx
@@ -0,0 +1,37 @@
+import { json } from "react-router-dom";
+import { isAuthorized } from "../../../common/helpers/auth";
+import { apiError, notAuthorized } from "../../../common/constants/errors/errors";
+import { useListCourseQuery } from "../../../common/queries/course.queries";
+
+export function Component() {
+    const { data, isLoading, isError, error} = useListCourseQuery();
+
+    if(isError){
+      json(
+        {message: error},
+        {status: 500,
+        statusText: apiError}
+      )
+    }
+
+
+}
+
+
+export async function loader(){
+
+    const value = await isAuthorized(["Admins"])
+
+    if(value){
+        return value;
+    } 
+
+    json({
+        message: notAuthorized
+    }, {
+        status: 401,
+        statusText: "UNAUTHORIZATION"
+    })
+}
+
+Component.displayName = "CourseEdit"
