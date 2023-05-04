80f647716ba95eb3363adf823951422190ad12ee
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2023-05-03 23:18:28 -0700

    Added Relevant changes

--- src/common/constants/navbars/navbar.ts
+++ src/common/constants/navbars/navbar.ts
@@ -3,7 +3,7 @@ import { Icon123, IconAbc, IconBuilding, IconUser } from "@tabler/icons-react";
 export const logout_link = "Logout"
 
 export const adminlinks = [
-    { link: '/admin/departments', label: 'Departments', icon: IconBuilding },
+    { link: '/admin/department', label: 'Departments', icon: IconBuilding },
     // { link: '/admin/courses', label: 'Courses', icon: IconAbc },
     // { link: '/admin/users', label: 'Users', icon: IconUser },
     // { link: '/admin/assignments', label: 'Assignments', icon: Icon123}
--- src/pages/Admin/main.tsx
+++ src/pages/Admin/main.tsx
@@ -2,15 +2,26 @@ import { Outlet, useNavigation } from "react-router-dom";
 import DashboardLayout from "../../layouts/dashboard/dashboard.layout";
 import { NavbarLayout } from "../../layouts/navbar/navbar.layout";
 import { adminlinks } from "../../common/constants/navbars/navbar";
-import { LoadingOverlay } from "@mantine/core";
+import { NavigationProgress, nprogress } from "@mantine/nprogress";
 
-export default function Main(){
-
+export default function Main() {
   const navigation = useNavigation();
-    
-    return(
-        <DashboardLayout navbar={<NavbarLayout data={adminlinks} />} header={<></>}>
-        {navigation.state !== "idle" ? <LoadingOverlay visible={true} overlayBlur={2} /> : <Outlet/>}
-        </DashboardLayout>
-    )
+
+  if (navigation.state !== "idle") {
+    nprogress.start();
+  } else {
+    nprogress.complete();
+  }
+
+  return (
+    <>
+      <NavigationProgress />
+      <DashboardLayout
+        navbar={<NavbarLayout data={adminlinks} />}
+        header={<></>}
+      >
+        <Outlet />
+      </DashboardLayout>
+    </>
+  );
 }
