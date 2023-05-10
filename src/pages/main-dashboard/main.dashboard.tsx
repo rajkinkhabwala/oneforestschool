2acfcfa9309a4e4a391a3f4040cff7467f559fd5
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2023-05-10 14:12:01 -0700

    changed admin folder

---
+++ src/pages/main-dashboard/main.dashboard.tsx
@@ -0,0 +1,31 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import DashboardLayout from "../../layouts/dashboard/dashboard.layout";
+import { NavbarLayout } from "../../layouts/navbar/navbar.layout";
+import { adminlinks } from "../../common/constants/navbars/navbar";
+import { NavigationProgress, nprogress } from "@mantine/nprogress";
+import { useEffect } from "react";
+
+export default function Main() {
+  const navigation = useNavigation();
+useEffect(() => {
+  
+  if (navigation.state !== "idle") {
+    nprogress.start();
+  } else {
+    nprogress.complete();
+  }
+}, [navigation.state])
+  
+
+  return (
+    <>
+      <NavigationProgress  />
+      <DashboardLayout
+        navbar={<NavbarLayout data={adminlinks} />}
+        header={<></>}
+      >
+        <Outlet />
+      </DashboardLayout>
+    </>
+  );
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,36 +1,37 @@
 import { createBrowserRouter, json } from "react-router-dom";
 import { isAuthorized } from "../common/helpers/auth";
 import logo from "../logo.svg";
-import AdminMain from "../pages/admin/main";
+import AdminMain from "../pages/admin-dashboard/main";
+import StudentMain from "../pages/main-dashboard/main.dashboard"
 // import mainMain from "../pages/main/main"
 import { notAuthorized } from "../common/constants/errors/errors";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    async loader() {
-      const auth = await isAuthorized(["Admins"]);
+    element: <StudentMain />,
+    loader: async() => {
+      const value = await isAuthorized(["Students"]);
 
-      return auth;
+      if (value.status) {
+        return value;
+      }else {
+        throw json(
+          {
+            message: notAuthorized,
+          },
+          {
+            status: 401,
+            statusText: "UNAUTHORIZATION",
+          }
+        );
+      }
     },
-    element: (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    ),
+    children: [
+      {
+        path: 'course/'
+      }
+    ]
   },
   {
     path: "admin/",
@@ -55,52 +56,52 @@ const router = createBrowserRouter([
     children: [
       {
         path: "department/",
-        lazy: () => import('../pages/admin/department/department.page'),
+        lazy: () => import('../pages/admin-dashboard/department/department.page'),
         
         children: [
           {
             path: ":id/",
             lazy: () =>
-              import("../pages/admin/department/singledepartment.page"),
+              import("../pages/admin-dashboard/department/singledepartment.page"),
           },
           {
             path: "create/",
-            lazy: () => import("../pages/admin/department/createdepartment.page"),
+            lazy: () => import("../pages/admin-dashboard/department/createdepartment.page"),
           },
           {
             path: "edit/",
-            lazy: () => import("../pages/admin/department/editdepartment.page"),
+            lazy: () => import("../pages/admin-dashboard/department/editdepartment.page"),
           }
         ],
       },
       {
         path: "course/",
-        lazy: () => import('../pages/admin/course/course.page'),
+        lazy: () => import('../pages/admin-dashboard/course/course.page'),
         
         children: [
           {
             path: ":id/",
             lazy: () =>
-              import("../pages/admin/course/singlecourse.page"),
+              import("../pages/admin-dashboard/course/singlecourse.page"),
           },
           {
             path: "create/",
-            lazy: () => import("../pages/admin/course/createcourse.page"),
+            lazy: () => import("../pages/admin-dashboard/course/createcourse.page"),
           },
           {
             path: "edit/",
-            lazy: () => import("../pages/admin/course/editcourse.page"),
+            lazy: () => import("../pages/admin-dashboard/course/editcourse.page"),
           }
         ],
       },
       {
         path: "notification/",
-        lazy: () => import('../pages/admin/notification/notification.page'),
+        lazy: () => import('../pages/admin-dashboard/notification/notification.page'),
 
         children: [
           {
             path: "create/",
-            lazy: () => import("../pages/admin/notification/createnotification.page")
+            lazy: () => import("../pages/admin-dashboard/notification/createnotification.page")
           }
         ]
       },
